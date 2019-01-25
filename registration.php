1a7d283a9b154295ebf9a8396c70d06aa49774c6
Sam <sam@mageplaza.com>
2019-01-25 17:11:21 +0700

    init

---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *                     Mageplaza_English United Kingdom package
+ *                     NOTICE OF LICENSE
+ *
+ *                     This source file is subject to the MIT License
+ *                     that is bundled with this package in the file LICENSE.txt.
+ *                     It is also available through the world-wide-web at this URL:
+ *                     https://www.mageplaza.com/LICENSE.txt
+ *
+ *                     @category  Mageplaza
+ *                     @package   Mageplaza_English United Kingdom
+ *                     @copyright Copyright (c) 2019
+ *                     @license   https://www.mageplaza.com/LICENSE.txt
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'mageplaza_en_gb',
+    __DIR__
+);
